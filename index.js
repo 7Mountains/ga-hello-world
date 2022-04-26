224ed1b84c7d5597aaeeb306de317fd898e7dc80
Rune Torkildsen <rto@7mountains.com>
2022-04-26 19:14:39 +0200

    Changes from hello-world to pr-payload

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  /**
+   * We need to fetch all the inputs that were provided to our action
+   * and store them in variables for us to use.
+   **/
+  const owner = core.getInput("owner", { required: true });
+  const repo = core.getInput("repo", { required: true });
+  const pr_number = core.getInput("pr_number", { required: true });
+  const token = core.getInput("token", { required: true });
+
+  console.log(`${owner}/${repo} (#${pr_number}) [${token}]`);
+
   const time = new Date().toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const payload = JSON.stringify(github.context.payload, 0, 2);
+  console.log(`Payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
